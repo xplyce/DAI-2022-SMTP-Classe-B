89f773a36c321d961d5326621073de4b18ac7176
Nicolas Philipp <nicolas.philipp@heig-vd.ch>
2022-11-14 14:44:23 +0100

    class Mail

---
+++ src/src/Mail.java
@@ -0,0 +1,12 @@
+
+public class Mail {
+
+    private Person sender;
+    private Group receivers;
+    private String subject;
+    private String text;
+
+    public Mail() {
+        receivers = new Group();
+    }
+}

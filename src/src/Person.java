6d60407e95eeb68bc3978e904f0dcf31f2c4b426
xplyce <xplicite.18@hotmail.com>
2022-11-14 13:44:27 +0100

    ajout classe Person

---
+++ src/src/Person.java
@@ -0,0 +1,24 @@
+public class Person {
+    private final String email;
+    private final String firstName;
+    private final String lastName;
+
+
+    public Person(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    String getEmail(){
+        return email;
+    }
+
+    String getFirstName(){
+        return firstName;
+    }
+
+    String getLastName(){
+        return lastName;
+    }
+}

08d1aeb9e8d430a8d4142693692059c05a65890b
xplyce <xplicite.18@hotmail.com>
2022-11-21 15:40:17 +0100

    Ajout des getter et setter dans la classe mail

--- src/src/model/mail/Mail.java
+++ src/src/model/mail/Mail.java
@@ -4,10 +4,27 @@ public class Mail {
 
     private Person sender;
     private Group receivers;
+    private Group cc;
     private String subject;
     private String text;
 
     public Mail() {
         receivers = new Group();
     }
+
+    public Person getSender() {return sender;}
+    public void setSender(Person sender){this.sender = sender;}
+
+    public Group getReceivers(){return receivers;}
+    public void setReceivers(Group receivers) {this.receivers = receivers;}
+
+    public Group getCc() {return cc;}
+    public void setCc(Group cc) {this.cc = cc;}
+
+    public String getSubject(){return subject;}
+    public void setSubject(String subject) {this.subject = subject;}
+
+    public String getText(){return text;}
+    public void setText(String text) {this.text = text;}
+
 }
